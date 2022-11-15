2454b5adad5473cdf7c56e8a37bbd6ecf4d401d4
coding4saudade <61851617+coding4saudade@users.noreply.github.com>
2022-11-15 11:00:13 -0600

    updated python statement for class

--- python/statements.py
+++ python/statements.py
@@ -4,7 +4,7 @@ lowered_input_name = input_name.lower()
 
 students = ['Amanzi', 'Angela', 'Holly'] #list of students for class
 students_lower=[] #empty list for changing list to lower case
-
+students_checker = []
 
 for name in students: #loop through student list
     name_lower = name.lower() #create variable to hold lower case names
@@ -15,11 +15,18 @@ for name in students: #loop through student list
     # print(students_lower)
     # print(" ")
 
-#     if len(students_lower) == len(students):
-    if lowered_input_name in students_lower: #check if input name is in the list
-                    
-        print("Hello, " + input_name)  #print hello statement
-        break     
-    else:
-        print("Sorry " + input_name + ", You shall not pass! Go back to the shadow") #if name is not in list, reject them
-        break    
+    if len(students_lower) == len(students): #when students_lower is the same size as student progress on
+        for x in students_lower:               #Loop through students_lower
+            
+            if x == lowered_input_name:          #if you find lower_input_name, print statement
+                print("Hi! " + input_name)
+                break
+            if lowered_input_name not in students_lower: #if input name is not found, print reject statement
+                print("Sorry " + input_name + ", You shall not pass! Go back to the shadow") 
+                break
+
+    #I found this code below more effecient than the nested loop starting at line 19. but I wanted to use the nested loop per the assignment
+        #if lowered_input_name in students_lower:
+              #print("Hi! " + input_name)
+        #if lowered_input_name not in students_lower:
+            #print("Sorry " + input_name + ", You shall not pass! Go back to the shadow")
